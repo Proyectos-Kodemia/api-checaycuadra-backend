8eac7e264f849512ad977555e7aa7043f1225d49
Miguel.Contreras <contreras.izquierdo.miguel@gmail.com>
2021-12-13 23:44:52 -0600

    Adding score in comments schema

--- models/meeting.js
+++ models/meeting.js
@@ -1,66 +1,64 @@
 const mongoose = require('mongoose')
 const DocumentSchema = require('./utils/documentos')
 const { Schema } = mongoose
-const Comment = mongoose.model("Comment")
+const Comment = mongoose.model('Comment')
 
 const schema = new Schema({
   user: {
-		 type: Schema.Types.ObjectId, 
+    type: Schema.Types.ObjectId,
     ref: 'Usuario'
   },
   userAccount: {
     type: String,
     required: true,
     minlength: 1
-	  },
+  },
   Date: {
     type: Date,
     required: true,
     minlength: 1
-	  },
+  },
   link: {
     type: String,
     required: true,
     minlength: 1
-	  },
+  },
   time: {
     type: String,
     required: true,
     minlength: 1
-	  },
+  },
   service: {
     type: String,
     required: true,
     minlength: 1
-	  },
+  },
   total: {
     type: String,
     required: false,
     minlength: 1
-	  },
-  
+  },
+
   documents: [DocumentSchema],
-  chat: {}
-},
+  chat: {},
 
-meetingComment:[{ 
-  type: Schema.Types.ObjectId, 
-  ref: 'Comment' 
-}]
-
-{
+  meetingComment: [{
+    type: Schema.Types.ObjectId,
+    ref: Comment
+  }]
+}, {
   timestamp: true
 })
 
 // {
-// 	usuario: 'ObjectId(wereyy234565434re)',
-// 	hora: '',
-// 	fecha: '',
-// 	documents: [
-// 		{
+// usuario: 'ObjectId(wereyy234565434re)',
+// hora: '',
+// fecha: '',
+// documents: [
+// {
 
-// 		}
-// 	]
+// }
+// ]
 // }
 
 module.exports = {
