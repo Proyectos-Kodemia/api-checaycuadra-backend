46d625858d6dba7a48edafc02f927a2a5b279642
Israeli Perez <israeli.ps@gmail.com>
2022-02-03 00:09:18 -0600

    registro usuario contador

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -67,10 +67,8 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const accountData = req.body
-    console.log(accountData)
     const accountCreated = await account.create(accountData)
     const { _id } = accountCreated
-    console.log('cuenta creada', accountCreated)
     res.status(201).json({
       status: true,
       message: 'Account Created Succesfully',
