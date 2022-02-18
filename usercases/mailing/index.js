6377ea31e4cea85e0337c0f086a9fc3ce5d20fa1
Israeli Perez <israeli.ps@gmail.com>
2022-02-18 00:19:45 -0600

    funcion de mailing completa

---
+++ usercases/mailing/index.js
@@ -0,0 +1,28 @@
+const config = require('../../lib/config')
+const sgMail = require('@sendgrid/mail')
+
+const mail = config.mailing.secretKey
+sgMail.setApiKey(mail)
+
+const sendMail = async ({ to, subject, text, html }) => {
+  const msg = {
+    to,
+    from: 'shadowhechicero@hotmail.com',
+    subject,
+    text,
+    html
+  }
+  const response = await sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+      return true
+    })
+    .catch((error) => {
+      console.error('error desde sendMail', error)
+      return false
+    })
+  return response
+}
+
+module.exports = { sendMail }
